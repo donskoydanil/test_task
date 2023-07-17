c02e87072500cc9bd1db439564f2c27722cc940e
Данил Донской <danil_donskoy@MacBook-Air-Danil.local>
2023-07-18 01:59:00 +0800

    fix name varname ARCHIVES_DIRECTORY

--- myapp/settings/services_settings.py
+++ myapp/settings/services_settings.py
@@ -3,4 +3,4 @@ from pathlib import Path
 
 
 BASE_DIRECTORY = Path(__file__).absolute().parent.parent
-ARCGIVES_DIRECTORY = BASE_DIRECTORY / 'archives'
+ARCHIVES_DIRECTORY = BASE_DIRECTORY / 'archives'
