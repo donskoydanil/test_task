7c5d3844696c10b1d62e0404c09d14f4c2350b28
Данил Донской <danil_donskoy@MacBook-Air-Danil.local>
2023-07-20 00:31:19 +0800

    inerface for path manager

--- myapp/services/core_logic/pathmanager/interface_path_manager.py
+++ myapp/services/core_logic/pathmanager/interface_path_manager.py
@@ -1,7 +1,14 @@
 import abc 
+from typing import Iterable,Optional
+
+
 
 
 
 class IPathManager(abc.ABC):
 
-    def 
+    
+    @abc.abstractmethod
+    def construct_path_from_parts(self, parts_of_path:Iterable[str]) -> str:
+        raise NotImplemented
+
