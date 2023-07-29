f684f14ab2ee09cb7c29f6166479aa6114c67e00
danil <trrripled@vk.com>
2023-07-30 01:09:03 +0800

    new func and remove dependency on os.path

--- myapp/services/core_logic/directory_creator/directory_creator.py
+++ myapp/services/core_logic/directory_creator/directory_creator.py
@@ -1,4 +1,4 @@
-import os
+import pathlib
 from typing import Iterable
 from myapp.services.core_logic.directory_creator.interface_directory_creator import IDirectoryCreator
 from myapp.services.core_logic.pathmanager.interface_path_manager import IPathManager 
@@ -9,14 +9,15 @@ class DirectoryCreator(IDirectoryCreator):
     def __init__(self,path_manager : IPathManager ) -> None:
         self.path_manager = path_manager
         
-
-
+    def _check_path_exist(self,path: pathlib.PurePosixPath) -> bool:
+        return self.path_manager.path_exists(path)
+    
     def create_direcory(self, parts_of_path: Iterable[str]) -> None:
         
         direcory_to_create = self.path_manager.construct_path_from_parts(parts_of_path)
 
-        if not os.path.exists(direcory_to_create):
-             os.makedirs(direcory_to_create)
+        if not self._check_path_exist(direcory_to_create):
+             direcory_to_create.mkdir()
         
 
 
