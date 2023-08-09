eb9a2d532c98e81d059ff6f08afc8abc17c6c5a5
danil <trrripled@vk.com>
2023-08-09 14:50:39 +0800

    status file descriptor

---
+++ myapp/domain/files/files_descriptors/status_descriptor/status_file_descriptor.py
@@ -0,0 +1,21 @@
+from typing import Any, Type
+from myapp.domain.files.file import File
+from myapp.domain.files.files_descriptors.base_descriptor.base_file_descriptor import FileBaseDescriptor
+
+
+
+class FileStatusDescriptor(FileBaseDescriptor):
+
+    def __init__(self, attribute_to_change_with_status: str,
+                 attribute_value: str,
+                 status_flag: str) -> None:
+
+        self.attribute_to_change = attribute_to_change_with_status  
+        self.attribute_value = attribute_value
+        self.status_flag = status_flag
+
+    def __set__(self, instance: Type[File], value: Any) -> None: 
+        if value == self.status_flag:
+            instance.__dict__[self.attribute_to_change] = self.attribute_value
+        super().__set__(instance, value)
+
