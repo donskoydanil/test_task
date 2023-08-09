b95b71ce07cfe73a07fe7e07d10b11a254adfbb0
danil <trrripled@vk.com>
2023-08-09 13:17:39 +0800

    inherit init logic from the base interface

--- myapp/infrastructure/container_interfaces/add_interface/container_add_interface.py
+++ myapp/infrastructure/container_interfaces/add_interface/container_add_interface.py
@@ -1,16 +1,11 @@
 from myapp.infrastructure.container.container import Container
+from myapp.infrastructure.container_interfaces.base_interface.base_container_interface import ContainerBaseInterface
 from myapp.domain.files.file import File
 from typing import Type
 
 
 
-class ContainerAddInterface:
-
-    def __init__(self, container:Type[Container]) -> None:
-        if container is None:
-            container = Container()
-
-        self.container = container
+class ContainerAddInterface(ContainerBaseInterface):
 
     def add(self,id:str,file:Type[File]) -> None:
         self.container.memory[id] = file
