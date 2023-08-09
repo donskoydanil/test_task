2476a27dd051aaa403240f1aefb34791fa8e51e6
danil <trrripled@vk.com>
2023-08-09 12:14:00 +0800

    placing init logic in a separate base class

---
+++ myapp/infrastructure/container_interfaces/add_interface/container_add_interface.py
@@ -0,0 +1,11 @@
+from myapp.infrastructure.container.container import Container
+from myapp.infrastructure.container_interfaces.base_interface.container_base_interface import ContainerBaseInterface
+from myapp.domain.files.file import File
+from typing import Type
+
+
+
+class ContainerAddInterface(ContainerBaseInterface):
+
+    def add(self,id:str,file:Type[File]) -> None:
+        self.container.memory[id] = file
