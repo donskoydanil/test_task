adfe627fda13b1b45de3cf781658d62df9486376
danil <trrripled@vk.com>
2023-08-09 15:11:26 +0800

    file DTO class

--- myapp/domain/files/file.py
+++ myapp/domain/files/file.py
@@ -1,12 +1,42 @@
+import json
+from typing import Dict
 from dataclasses import dataclass
+from myapp.domain.files.files_descriptors.status_descriptor.status_file_descriptor import FileStatusDescriptor
 
 
 @dataclass
-class IFile:
-    file_name : str
-    format : str
-    status : str
-    progress : str 
+class File:
+    
+    progress : str
+    files : list = []
+    _is_status_ok : bool = False
+    status : FileStatusDescriptor = FileStatusDescriptor('_is_status_ok',True,'ok')
+
+
+    def _info_status_and_progress(self)->Dict[str,str]:
+        return {
+            'status': self.status,
+            'progress' : self.progress 
+        }
+    
+    def _info_files(self)->Dict[str,str]:
+        
+        return {
+            'files' : self.files
+        }
+    
+    def out_info(self) -> str:
+        out_for_json = self._info_status_and_progress()
+
+        if self._is_status_ok:
+            out_for_json.update(self._info_files())
+
+        return json.dumps(out_for_json)
+        
+
+
+
+
 
     
 
