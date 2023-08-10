97763383bdb0817c28a54d5a72319aa303aefc36
danil <trrripled@vk.com>
2023-08-10 15:54:31 +0800

    add Optioanl typing

--- myapp/infrastructure/container_interfaces/base_interface/base_container_interface.py
+++ myapp/infrastructure/container_interfaces/base_interface/base_container_interface.py
@@ -1,12 +1,12 @@
 from myapp.infrastructure.container.container import Container
-from typing import Type
+from typing import Type,Optional
 
 
 
 
 class ContainerBaseInterface:
 
-    def __init__(self, container:Type[Container]) -> None:
+    def __init__(self, container:Optional[Type[Container]] = None) -> None:
         if container is None:
             container = Container()
 
