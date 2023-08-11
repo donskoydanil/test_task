ad4bc79fd79ca8a87aa8d0b9bc472234b6a4d34a
danil <trrripled@vk.com>
2023-08-11 19:43:14 +0800

    fix typing

--- myapp/services/core_logic/bulders/bulder_download_with_procent/bulder_download_with_procent.py
+++ myapp/services/core_logic/bulders/bulder_download_with_procent/bulder_download_with_procent.py
@@ -1,8 +1,7 @@
-
-from typing import Type
 from myapp.services.core_logic.bulders.bulder_interface.base_bulder import BaseBulder
 from myapp.services.core_logic.downloader_file.downloader_file_with_procents import DownloadFileWithProcents
 from myapp.infrastructure.files_interfaces.progress_interface.file_progress_interface import FileProgressInterface
+from myapp.services.core_logic.downloader_file.interface_downloader_file import IDownloaderFile
 
 class DownloadWithProcentBuilder(BaseBulder):
 
@@ -10,5 +9,5 @@ class DownloadWithProcentBuilder(BaseBulder):
         self.file_progress_interface = file_progress_interface
 
     @property
-    def ready_instanse(self) -> Type[DownloadFileWithProcents[FileProgressInterface]]:
+    def ready_instanse(self) -> IDownloaderFile:
         return DownloadFileWithProcents(self.file_progress_interface)
