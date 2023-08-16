409b6e42c1d729dbf2b7235cf81d318ef1c01e50
danil <trrripled@vk.com>
2023-08-16 11:47:57 +0800

    fix typing

--- myapp/services/core_logic/bulders/bulder_download_by_link/bulder_download_by_link.py
+++ myapp/services/core_logic/bulders/bulder_download_by_link/bulder_download_by_link.py
@@ -1,16 +1,15 @@
-from typing import Type
 from myapp.infrastructure.clientsession.clientsession_interface import ClientSessionInterface
 from myapp.services.core_logic.bulders.bulder_interface.base_bulder import BaseBulder
 from myapp.services.core_logic.downloader_by_link.downloader_by_link import DownloadByLink
 from myapp.services.core_logic.downloader_by_link.interface_dowloader_by_link import IDownloaderByLiknk
-from myapp.services.core_logic.downloader_file.downloader_file_with_procents import DownloadFileWithProcents
-from myapp.infrastructure.files_interfaces.progress_interface.file_progress_interface import FileProgressInterface
+from myapp.services.core_logic.downloader_file.interface_downloader_file import IDownloaderFile
+
 
 class DownloadByLinkBulder(BaseBulder):
 
     def __init__(self, 
                 session_maker : ClientSessionInterface, 
-                dowloader_with_procent:Type[DownloadFileWithProcents[FileProgressInterface]]
+                dowloader_with_procent : IDownloaderFile
                 ) -> None:
         self.session = session_maker.make_session
         self.dowloader_with_procent_instance = dowloader_with_procent
