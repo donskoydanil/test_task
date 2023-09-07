099fdf545447fe611bfd18712947f31e6ca3d328
danil <trrripled@vk.com>
2023-09-07 12:28:24 +0800

    delete direcory class

---
+++ myapp/services/core_logic/directory_manilulations/delete.py
@@ -0,0 +1,17 @@
+import shutil
+from typing import Iterable
+from base import IDirectoryDeleter
+from myapp.services.core_logic.pathmanager.interface_path_manager import IPathManager 
+
+
+class DirectoryDeleter(IDirectoryDeleter):
+
+    def __init__(self,path_manager : IPathManager ) -> None:
+        self.path_manager = path_manager
+
+    def delete_direcory(self, parts_of_path: Iterable[str]) -> None:
+        
+        direcory_to_delete = self.path_manager.construct_path_from_parts(parts_of_path)
+
+        if self.path_manager.path_exists(direcory_to_delete):
+            shutil.rmtree(direcory_to_delete)
