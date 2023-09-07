45ef2110fee37f5d35b580f146c3d9298a178a2c
danil <trrripled@vk.com>
2023-09-07 12:28:04 +0800

    create directory class

---
+++ myapp/services/core_logic/directory_manilulations/create.py
@@ -0,0 +1,17 @@
+from typing import Iterable
+from base import IDirectoryCreator
+from myapp.services.core_logic.pathmanager.interface_path_manager import IPathManager 
+
+
+class DirectoryCreator(IDirectoryCreator):
+
+    def __init__(self,path_manager : IPathManager ) -> None:
+        self.path_manager = path_manager
+        
+    def create_direcory(self, parts_of_path: Iterable[str]) -> None:
+        
+        direcory_to_create = self.path_manager.construct_path_from_parts(parts_of_path)
+
+        if not self.path_manager.path_exists(direcory_to_create):
+             direcory_to_create.mkdir()
+        
