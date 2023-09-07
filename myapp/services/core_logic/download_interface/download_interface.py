0a62ece35fd54afd2c62565c81faf42e69404d2b
danil <trrripled@vk.com>
2023-09-07 12:38:53 +0800

    delete download interface class

--- myapp/services/core_logic/download_interface/download_interface.py
+++
@@ -1,9 +0,0 @@
-import pathlib
-from myapp.services.core_logic.downloader_by_link.interface_dowloader_by_link import IDownloaderByLiknk
-class DownloadInterafce:
-
-    def __init__(self, download_link_instace:IDownloaderByLiknk) -> None:
-        self.download_link_instace = download_link_instace
-    
-    async def download(self, url:str, path:pathlib.Path):
-        await self.download_link_instace.download(url,path)
