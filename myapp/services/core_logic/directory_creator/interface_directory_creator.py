7fc86693da970ac3487c22ef41954b49725b2b17
danil <trrripled@vk.com>
2023-09-07 12:26:12 +0800

    delete old version of directory_actions classes

--- myapp/services/core_logic/directory_creator/interface_directory_creator.py
+++
@@ -1,9 +0,0 @@
-import abc
-from typing import Iterable
-
-
-class IDirectoryCreator(abc.ABC):
-
-    @abc.abstractmethod
-    def create_direcory(self, parts_of_path: Iterable[str]) -> None:
-        return NotImplemented
--- myapp/services/core_logic/directory_deleter/interface_directory_deliter.py
+++
@@ -1,9 +0,0 @@
-import abc
-from typing import Iterable
-
-
-class IDirectoryDeleter(abc.ABC):
-
-    @abc.abstractmethod
-    def delete_direcory(self, parts_of_path: Iterable[str]) -> None:
-        return NotImplemented
